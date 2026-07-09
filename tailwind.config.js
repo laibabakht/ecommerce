839d7727989ea92f3bd054ce812c9c8638ca5d03
laibabakht <laibabakht786@gmail.com>
2026-07-09 15:47:17 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+
+export default {
+  content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}'
+],
+  theme: {
+    extend: {
+      colors: {
+        rose: {
+          50: '#fff1f2',
+          100: '#ffe4e6',
+          200: '#fecdd3',
+          300: '#fda4af',
+          400: '#fb7185',
+          500: '#f43f5e',
+          600: '#e11d48',
+          700: '#be123c',
+          800: '#9f1239',
+          900: '#881337',
+        },
+        cream: {
+          DEFAULT: '#FFF8F0',
+          50: '#FFFCFA',
+          100: '#FFF8F0',
+          200: '#FFEED9',
+        }
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
